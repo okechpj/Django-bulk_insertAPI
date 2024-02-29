d5d451e10dcde0b53d1efc5fad663f39168f2844
peter <okechpj@gmail.com>
2024-02-29 12:37:27 +0300

    changed primary keys

--- core/models.py
+++ core/models.py
@@ -2,26 +2,28 @@ from django.db import models
 
 
 
+# create a products model
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    product_image = models.ImageField(upload_to="core_images")
+    
+    
+
+    def __str__(self):
+        return self.product_name
+
+
+
 # Create a product variant model
 class Product_variant(models.Model):
     product_variant_sku = models.CharField(max_length=100, unique=True)
     product_variant_name = models.CharField(max_length=255)
     product_variant_price = models.DecimalField(max_digits=10, decimal_places=2)
     product_variant_details = models.TextField()
-   
+     # create a many to one relationship with Product model
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)
   
 
     def __str__(self):
         return self.product_variant_name
     
-
-# create a products model
-class Product(models.Model):
-    product_name = models.CharField(max_length=255)
-    product_image = models.ImageField(upload_to="core_images")
-     # create a many to one relationship with Product_variant model
-    product_variant = models.ForeignKey(Product_variant, on_delete=models.CASCADE, blank=True, null=True)
-
-    def __str__(self):
-        return self.product_name
-
--- store_api/serializers.py
+++ store_api/serializers.py
@@ -2,31 +2,34 @@ from rest_framework import serializers # import restframework
 from core.models import Product, Product_variant # import models from core app
 
 
-# create productVariant serializer to serialize and deserialize instance of Product_variant model
-class ProductVariantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product_variant
-        # select the fields that are to be displayed
-        fields = ['id','product_variant_sku', 'product_variant_name', 'product_variant_price', 'product_variant_details']
-        
 
 # create productSerializer to serialize and deserialize instance of Product model
 class ProductSerializer(serializers.ModelSerializer):
     product_image = serializers.CharField(allow_null=True, required=False) # allow null value for image fields
-    product_variant = ProductVariantSerializer() # Nesting Product_variant serializer in Product serializer
-
-    def create(self, validated_data):   #create method extracts and processes only validated data
-        product_variant_data = validated_data.pop('product_variant')  #  reomove product_variant data from validated data
-        product = Product.objects.create(**validated_data)  # create a new instance of Product model
-        product_variant = Product_variant.objects.create(product=product, **product_variant_data)  #create new instance of Product_variant using the extracted product_variant_data, associating it with newly created Product instance
-        product.product_variant = product_variant  # 
-        product.save() #saves the Product instance with the associated Product_variant 
-        return product  # returns product instance
-    
     class Meta:
         model = Product # model to be used
         #select fields to be displayed
-        fields = ['id','product_name', 'product_image', 'product_variant']
+        fields = ['id','product_name', 'product_image']
+
+# create productVariant serializer to serialize and deserialize instance of Product_variant model
+class ProductVariantSerializer(serializers.ModelSerializer):
+    product = ProductSerializer() # Nesting Product serializer in Product_variant serializer
+
+    def create(self, validated_data):   #create method extracts and processes only validated data
+        product_data = validated_data.pop('product')  #  reomove product_variant data from validated data
+        product_variant = Product_variant.objects.create(**validated_data)  # create a new instance of Product_variant model
+        product = Product.objects.create(product_variant=product_variant, **product_data)  #create new instance of Product using the extracted product_data, associating it with newly created Product_variant instance
+        product_variant.product = product  # 
+        product_variant.save() #saves the Product_variant instance with the associated Product
+        return product_variant  # returns product_variant instance
+    
+      
+    class Meta:
+        model = Product_variant
+        # select the fields that are to be displayed
+        fields = ['id','product_variant_sku', 'product_variant_name', 'product_variant_price', 'product_variant_details', 'product']
+        
+
         
 
 
